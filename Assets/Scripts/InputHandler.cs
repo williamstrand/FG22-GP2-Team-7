ee7ff87fd6a7254a9b715724e748568a74a8ac36
williamstrand <william.strand.paurell@edu.futuregames.nu>
2023-01-31 14:55:47 +0100

    Update InputHandler.cs
    Made it so that you can see if the interact button is being held

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -8,6 +8,7 @@ public class InputHandler : MonoBehaviour
     public Action OnPlayerAction;
 
     public Vector2 MoveInput { get; private set; }
+    public bool InteractButtonHeld { get; private set; }
 
 
     Controls _controls;
@@ -18,6 +19,8 @@ public class InputHandler : MonoBehaviour
         _controls.Player.Jump.performed += _ => OnJump?.Invoke();
         _controls.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
         _controls.Player.Interact.performed += _ => OnInteract?.Invoke();
+        _controls.Player.Interact.performed += _ => InteractButtonHeld = true;
+        _controls.Player.Interact.canceled += _ => InteractButtonHeld = false;
         _controls.Player.PlayerAction.performed += _ => OnPlayerAction?.Invoke();
         _controls.Enable();
     }
