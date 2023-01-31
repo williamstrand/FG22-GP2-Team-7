c8126d1911c499c7c3f1663e51d605200d415bdd
williamstrand <william.strand.paurell@edu.futuregames.nu>
2023-01-31 15:06:04 +0100

    Added gamepad controls

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -17,10 +17,13 @@ public class InputHandler : MonoBehaviour
     {
         _controls = new Controls();
         _controls.Player.Jump.performed += _ => OnJump?.Invoke();
+        
         _controls.Player.Move.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        
         _controls.Player.Interact.performed += _ => OnInteract?.Invoke();
         _controls.Player.Interact.performed += _ => InteractButtonHeld = true;
         _controls.Player.Interact.canceled += _ => InteractButtonHeld = false;
+        
         _controls.Player.PlayerAction.performed += _ => OnPlayerAction?.Invoke();
         _controls.Enable();
     }
