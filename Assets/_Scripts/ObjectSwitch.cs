e22a0f591a67229366182cc6dd56b12e7cf4003c
Tarik Ahmed <tarik@tmggames.io>
2023-02-02 17:14:49 +0100

    Made changes in Object Switch script

--- Assets/_Scripts/ObjectSwitch.cs
+++ Assets/_Scripts/ObjectSwitch.cs
@@ -1,27 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSwitch : MonoBehaviour
 {
     [SerializeField] private GameObject _object;
     [SerializeField] private TriggerController _trigger;
-    [SerializeField] private bool revealWhenPressed;
-    [SerializeField] private bool fadeIn;
-    [SerializeField] private float fadeTime = 1f;
+    [SerializeField] private bool revealWhenPressed = true;
+    [SerializeField] private bool fadeInOut = false;
+    [SerializeField] private float fadeDuration = 1f;
 
     private MeshRenderer _meshRenderer;
-    private Material _material;
     private Color _startColor;
     private Color _endColor;
-    private bool _fading;
+    private float _timeStartedLerping;
 
     private void Start()
     {
         _meshRenderer = _object.GetComponent<MeshRenderer>();
-        _material = _meshRenderer.material;
-        _startColor = _material.color;
-        _endColor = new Color(_startColor.r, _startColor.g, _startColor.b, revealWhenPressed ? (fadeIn ? 1 : 0) : (fadeIn ? 0 : 1));
+        _startColor = _meshRenderer.material.color;
+        _endColor = revealWhenPressed ? new Color(_startColor.r, _startColor.g, _startColor.b, 1f) : new Color(_startColor.r, _startColor.g, _startColor.b, 0f);
     }
 
     private void Update()
@@ -31,23 +27,44 @@ public class ObjectSwitch : MonoBehaviour
 
     private void SwitchObject()
     {
-        if (_trigger.isPressed && !_fading)
+        if (_trigger.isPressed)
         {
-            StartCoroutine(FadeObject());
+            if (fadeInOut)
+            {
+                _timeStartedLerping = Time.time;
+                FadeInOutObject();
+            }
+            else
+            {
+                _object.SetActive(revealWhenPressed);
+            }
+        }
+        else
+        {
+            if (fadeInOut)
+            {
+                _timeStartedLerping = Time.time;
+                FadeInOutObject();
+            }
+            else
+            {
+                _object.SetActive(!revealWhenPressed);
+            }
         }
     }
 
-    private IEnumerator FadeObject()
+    private void FadeInOutObject()
     {
-        _fading = true;
-        float elapsedTime = 0f;
-        while (elapsedTime < fadeTime)
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / fadeDuration;
+
+        
+
+        if (percentageComplete >= 1)
         {
-            elapsedTime += Time.deltaTime;
-            _material.color = Color.Lerp(_startColor, _endColor, elapsedTime / fadeTime);
-            yield return null;
+            _object.SetActive(revealWhenPressed);
+            return;
         }
-        _material.color = _endColor;
-        _fading = false;
+        _meshRenderer.material.color = Color.Lerp(_startColor, _endColor, percentageComplete);
     }
 }
