efa273edd4d1fa8a6624d178d897b46f67f122b1
Tarik Ahmed <tarik@tmggames.io>
2023-02-08 16:38:18 +0100

    Major Commit - Tools and Shaders
    - Refined the grass editor tool to allow for terrain blending
    - Added textures for testing with the interactive terrain shaders
    - Added a "fake liquid" shader
    - Added a skybox shader with day/night cycle
    - Created prefabs out of grass tool and effect cameras
    - Deleted unnecessary test assets

--- Assets/_Scripts/SetInteractiveShaderEffects.cs
+++ Assets/_Scripts/SetInteractiveShaderEffects.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class SetInteractiveShaderEffects : MonoBehaviour
 {
     [SerializeField]
-    RenderTexture _renderTexture;
+    RenderTexture rt;
     [SerializeField]
-    Transform _target;
+    Transform target;
     // Start is called before the first frame update
     void Awake()
     {
-        Shader.SetGlobalTexture("_GlobalEffectRT", _renderTexture);
+        Shader.SetGlobalTexture("_GlobalEffectRT", rt);
         Shader.SetGlobalFloat("_OrthographicCamSize", GetComponent<Camera>().orthographicSize);
     }
-
+ 
     private void Update()
     {
-        transform.position = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
         Shader.SetGlobalVector("_Position", transform.position);
     }
-
-
+ 
+ 
 }
