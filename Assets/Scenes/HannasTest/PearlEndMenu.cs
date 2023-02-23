d17c896b8db20dabbc98e5f6f6cf23fbae05a0d8
abramshanna <hanna_96eri@hotmail.com>
2023-02-23 12:50:18 +0100

    script fix

--- Assets/Scenes/HannasTest/PearlEndMenu.cs
+++ Assets/Scenes/HannasTest/PearlEndMenu.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PearlEndMenu : MonoBehaviour
 {
-    public UIManager uIManager;
     public int scene;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == ("Player"))
         {
-            uIManager.FadeToScene(scene);
+            UIManager.instance.FadeToScene(scene);
         }
     }
 }
