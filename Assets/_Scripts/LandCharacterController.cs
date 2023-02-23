a40f2d10d230846f614f71ecc903dfc50ff73151
williamstrandp <strandenpaurell@outlook.com>
2023-02-23 17:00:59 +0100

    Update LandCharacterController.cs

--- Assets/_Scripts/LandCharacterController.cs
+++ Assets/_Scripts/LandCharacterController.cs
@@ -85,7 +85,6 @@ public class LandCharacterController : CharacterController
 
     protected override void Jump()
     {
-        Debug.Log("Jump");
         switch (_playerState)
         {
             case LandPlayerState.Default:
